7ea718beb5ebf6126c0ec0688b40684ce2e1a548
Colin <colinnlsn@gmail.com>
2016-09-21 09:48:16 -0500

    Added Chapter 5
    Added Chapter 5 of the Dao of Programming

--- index.js
+++ index.js
@@ -179,7 +179,44 @@ var TAOS = [
     "I then compile the program. I sit still and let the joy of the work fill my being. "+
     "I close my eyes for a moment and then log off. "+
     "Prince Wang said, Would that all of my programmers were as wise!",
-    
+
+    "Thus spake the Master Programmer: " +
+    "Though a program be but three lines long, someday it will have to be maintained.",
+
+    "A well-used door needs no oil on its hinges. " +
+    "A swift-flowing stream does not grow stagnant. " +
+    "A deer blends perfectly into the forest colors. "+
+    "Software rots if not used. "+
+    "These are great mysteries.",
+
+    "A manager asked a programmer how long it would take him to finish the program on which he was working. "+
+    "I will be finished tomorrow, the programmer promptly replied. "+
+    "I think you are being unrealistic, said the manager, Truthfully, how long will it take?" +
+    "The programmer thought for a moment. I have some features that I wish to add. "+
+    "This will take at least two weeks, he finally said. "+
+    "Even that is too much to expect, insisted the manager, "+
+    "I will be satisfied if you simply tell me when the program is complete."+
+    "The programmer agreed to this. "+
+    "Several years later, the manager retired. "+
+    "On the way to his retirement luncheon, he discovered the programmer asleep at his terminal. "+
+    "He had been programming all night.",
+
+    "A novice programmer was once assigned to code a simple financial package."+
+    "The novice worked furiously for many days, "+
+    "but when his Master reviewed his program, "+
+    "he discovered it contained a screen editor, "+
+    "a set of generalized graphics routines, "+
+    "and an artificial intelligence interface, but not the slightest hint of anything financial. "+
+    "When the Master asked about this, the novice became indignant. " +
+    "Don't be so impatient, he said, I'll put in the financial stuff eventually.",
+
+    "Does a good farmer neglect a crop he has planted? "+
+    "Does a good teacher overlook even the most humble student? "+
+    "Does a good father allow a single child to starve? "+
+    "Does a good programmer refuse to maintain his code?",
+
+    "",
+    "",
     "",
     "",
     ""
