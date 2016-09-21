690d8eeaf860035077aa6aba8f4eb4ace523c24c
Colin <colinnlsn@gmail.com>
2016-09-21 11:57:09 -0500

    Added Book 6
    Added Book 6 of the Tao of Programming

--- index.js
+++ index.js
@@ -215,6 +215,47 @@ var TAOS = [
     "Does a good father allow a single child to starve? "+
     "Does a good programmer refuse to maintain his code?",
 
+    "Thus spake the Master Programmer: " +
+    "Let the programmers be many and the managers few -- then all will be productive.",
+
+    "When managers hold endless meetings, the programmers write games. " +
+    "When accountants speak of quarterly profits, the development budget is about to be cut. " +
+    "When senior scientists talk blue sky, the clouds are about to roll in. "+
+    "Truly, this is not the Tao of Programming. "+
+    "When managers make commitments, game programs are ignored. " +
+    "When accountants make long-range plans, harmony and order are about to be restored. " +
+    "When senior scientists address the problems at hand, the problems will soon be solved. " +
+    "Truly, this is the Tao of Programming.",
+
+    "Why are programmers non-productive? Because their time is wasted in meetings. " +
+    "Why are programmers rebellious? Because the management interferes too much. "+
+    "Why are the programmers resigning one by one? Because they are burnt out. " +
+    "Having worked for poor management, they no longer value their jobs.",
+
+    "A manager was about to be fired, but a programmer who worked for him wrote a new program that became popular and sold well. "+
+    "As a result, the manager retained his job. "+
+    "The manager tried to give the programmer a bonus, but the programmer refused it, saying, "+
+    "I wrote the program because I thought it was an interesting concept, and thus I expect no reward. "+
+    "The manager upon hearing this remarked, "+
+    "This programmer, though he holds a position of small esteem, "+
+    "understands well the proper duty of an employee. "+
+    "Let us promote him to the exalted position of management consultant!" +
+    "But when told this, the programmer once more refused, saying, "+
+    "I exist so that I can program. "+
+    "If I were promoted, I would do nothing but waste everyone's time. "+
+    "Can I go now? I have a program that I am working on.",
+
+    "A manager went to his programmers and told them: "+
+    "As regards to your work hours: "+
+    "you are going to have to come in at nine in the morning and leave at five in the afternoon." +
+    "At this, all of them became angry and several resigned on the spot. "+
+    "So the manager said: "+
+    "All right, in that case you may set your own working hours, as long as you finish your projects on schedule. " +
+    "The programmers, now satisfied, began to come in at noon and work to the wee hours of the morning.",
+    
+    "",
+    "",
+    "",
     "",
     "",
     "",
