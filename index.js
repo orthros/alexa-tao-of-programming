5a392b1be2c1588bcada80faf7a7467b470256b6
Colin <colinnlsn@gmail.com>
2016-09-21 09:10:26 -0500

    Added Book 4
    Added Book 4 to the list of quotes

--- index.js
+++ index.js
@@ -124,6 +124,62 @@ var TAOS = [
     "The Master Programmer frowned. In that case, it will take two years." +
     "And what if I assign a hundred programmers to it?" +
     "The Master Programmer shrugged. Then the design will never be completed, he said.",
+
+    "Thus spake the Master Programmer: "+
+    "A well-written program is its own Heaven; a poorly-written program is its own Hell.",
+
+    "A program should be light and agile, its subroutines connected like a string of pearls. "+
+    "The spirit and intent of the program should be retained throughout. "+
+    "There should be neither too little nor too much. Neither needless loops nor useless variables; "+
+    "neither lack of structure nor overwhelming rigidity. "+
+    "A program should follow the Law of Least Astonishment. "+
+    "What is this law? It is simply that the program should always respond to the users in the way that least astonishes them. "+
+    "A program, no matter how complex, should act as a single unit. " +
+    "The program should be directed by the logic within rather than by outward appearances. " +
+    "If the program fails in these requirements, it will be in a state of disorder and confusion. "+
+    "The only way to correct this is to rewrite the program.",
+
+    "A novice asked the Master: " +
+    "I have a program that sometimes runs and sometimes aborts. "+
+    "I have followed the rules of programming, yet I am totally baffled. What is the reason for this?" +
+    "The Master replied: "+
+    "You are confused because you do not understand Tao. "+
+    "Only a fool expects rational behavior from his fellow humans. " +
+    "Why do you expect it from a machine that humans have constructed? "+
+    "Computers simulate determinism; only Tao is perfect. "+
+    "The rules of programming are transitory; only Tao is eternal. "+
+    "Therefore, you must contemplate Tao before you receive Enlightenment." +
+    " But how will I know when I have received Enlightenment?" +
+    "asked the novice."+
+    "Your program will run correctly, replied the Master.",
+
+    "The Master was explaining the nature of Tao to one of his novices. "+
+    "The Tao is embodied in all software -- regardless of how insignificant, said the Master."+
+    "Is the Tao in a hand-held calculator? asked the novice. "+
+    "It is, came the reply. "+
+    "Is the Tao in a video game? asked the novice. " +
+    "It is even in a video game, said the Master. "+
+    "Is the Tao in the DOS for a personal computer? asked the novice. "+
+    "The Master coughed and shifted his position slightly. The lesson is over for today, he said.",
+
+    "Prince Wang's programmer was coding software. His fingers danced upon the keyboard. "+
+    "The program compiled without and error message, and the program ran like a gentle wind. "+
+    "Excellent! the Prince exclaimed. Your technique is faultless!" +
+    "Technique? said the programmer, turning from his terminal, "+
+    "What I follow is Tao -- beyond all techniques! "+
+    "When I first began to program, I would see before me the whole problem in one mass. "+
+    "After three years, I no longer saw this mass. "+
+    "Instead, I used subroutines. But now I see nothing. "+
+    "My whole being exists in a formless void. My senses are idle. "+
+    "My spirit, free to work without a plan, follows its own instinct. "+
+    "In short, my program writes itself. "+
+    "True, sometimes there are difficult problems. "+
+    "I see them coming, I slow down, I watch silently. "+
+    "Then I change a single line of code and the difficulties vanish like puffs of idle smoke. "+
+    "I then compile the program. I sit still and let the joy of the work fill my being. "+
+    "I close my eyes for a moment and then log off. "+
+    "Prince Wang said, Would that all of my programmers were as wise!",
+    
     "",
     "",
     ""
